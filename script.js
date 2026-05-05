8870f765abcb7de288e8444db8cd8ebe520f5b5e
Jihed Berlim <jihedberlim@gmail.com>
2026-05-05 17:13:33 -0300

    feat: add a menu closing event when the link is clicked

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const navbar = document.querySelector('.navbar')
-const navbarLinks = document.querySelector('.navbar__links')
-const button = document.querySelector('.burguer')
+const navbar = document.querySelector(".navbar");
+const navLinks = document.querySelector(".navbar__links");
+const button = document.querySelector(".burguer");
+const links = document.querySelectorAll(".navbar__links a");
 
-button.addEventListener('click', function() {
-    navbarLinks.classList.toggle('active')
-})
+button.addEventListener("click", function () {
+  navLinks.classList.toggle("active");
+});
 
-window.addEventListener('scroll', function() {
-    if(this.window.pageYOffset > 0) {
-        navbar.classList.add('active')
+links.forEach(function (link) {
+  link.addEventListener("click", function () {
+    navLinks.classList.remove("active");
+  });
+});
+
+window.addEventListener(
+  "scroll",
+  function () {
+    if (this.window.pageYOffset > 0) {
+      navbar.classList.add("active");
     } else {
-        navbar.classList.remove('active')
+      navbar.classList.remove("active");
     }
-})
+  },
+  { passive: true },
+);
